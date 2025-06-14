a853707b0020e0d0e3405e7cd46a0929e476cee0
Marjoy M. Caranto <marjoycaranto29@gmail.com>
2025-06-15 01:39:59 +0800

    Display logo in window title bar

--- frontend/main_window.py
+++ frontend/main_window.py
@@ -1,8 +1,20 @@
+import os
 from tkinter import *
+from PIL import Image, ImageTk
 
+# Make the script's folder the current directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Create the main window
 window = Tk()
 window.geometry("390x844")
-window.title("Move Out") # Service Name
+window.title("Move Out")  # Service Name
 
+# Logo image
+logo = Image.open(r"../media/logo.png")
+logo = logo.resize((10, 40)) 
+logo_img = ImageTk.PhotoImage(logo)
+
+window.iconphoto(True, logo_img)
 
 window.mainloop()
