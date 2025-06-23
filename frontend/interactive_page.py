f363c1917112611a71cebbcc7f5dadb4dfb076f1
Marjoy M. Caranto <marjoycaranto29@gmail.com>
2025-06-23 22:16:29 +0800

    Add docstring to InteractivePage class for clarity and documentation

--- frontend/interactive_page.py
+++ frontend/interactive_page.py
@@ -2,6 +2,10 @@ from tkinter import *
 import tkinter.font as tkFont
 
 class InteractivePage(Frame):
+    '''
+        This module provides the interactive page for Move Out.
+        It allows the user to navigate to the login and signup pages.
+    '''
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
