c4fca62bb3ab04a59d088d593cffad033baf7e42
Marjoy M. Caranto <marjoycaranto29@gmail.com>
2025-06-24 16:30:12 +0800

    Add navigation from login page to home page

--- frontend/main_window.py
+++ frontend/main_window.py
@@ -1,15 +1,17 @@
 import os
 from tkinter import *
-from PIL import Image, ImageTk 
+from PIL import Image, ImageTk
+
 from .splash_animation import Animation
 from .interactive_page import InteractivePage
 from .login_page import LoginPage
 from .signup_page import SignUpPage
+from .home_page import HomePage
 
 class MainWindow(Tk):
     '''
-        This module defines the MainWindow class for the Move Out application.
-        It initializes the main Tkinter window, displays all the pages
+    This module defines the MainWindow class for the Move Out application.
+    It initializes the main Tkinter window, displays all the pages.
     '''
     def __init__(self):
         super().__init__()
@@ -27,30 +29,43 @@ class MainWindow(Tk):
         logo_img = ImageTk.PhotoImage(logo)
         self.iconphoto(True, logo_img)
 
-        # Adding animation with callback to transition
+        # Show animation then go to interactive page
         self.logo_animation = Animation(self, callback=self.transition_to_interactive_page)
         self.logo_animation.pack(fill=BOTH, expand=True)
+
+    def clear_current_page(self):
+        # Hide all packed widgets (previous pages)
+        for widget in self.winfo_children():
+            widget.pack_forget()
 
     def transition_to_interactive_page(self):
         # Destroy the splash screen (removes the logo animation)
         self.logo_animation.destroy()
 
-        # Create and show the InteractivePage
+        # Show interactive page
         self.interactive_page = InteractivePage(self)
         self.interactive_page.pack(fill=BOTH, expand=True)
-    
-    def show_login_page(self):
-        # Go to login page
-        self.interactive_page.pack_forget()
 
-        # Create and show the login page
+    def show_login_page(self):
+        # Hide current page
+        self.clear_current_page()
+
+        # Create and show the log-in page
         self.login_page = LoginPage(self)
         self.login_page.pack(fill=BOTH, expand=True)
 
     def show_signup_page(self):
-        # Go to login page
-        self.interactive_page.pack_forget()
+        # Hide current page
+        self.clear_current_page()
 
-        # Create and show the login page
+        # Create and show the sign-up page
         self.signup_page = SignUpPage(self)
         self.signup_page.pack(fill=BOTH, expand=True)
+
+    def show_home_page(self):
+        # Hide current page
+        self.clear_current_page()
+
+        # Create and show the home page
+        self.home_page = HomePage(self)
+        self.home_page.pack(fill=BOTH, expand=True)
